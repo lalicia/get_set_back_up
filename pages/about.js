fbccd0debdb43cc5e1b925a0aeee5d22ee242ac9
alicialowe <hohpascal@outlook.com>
2022-10-23 12:40:49 +0100

    centered text on About page following successful deployment - doesn't look quite right uncentered

--- pages/about.js
+++ pages/about.js
@@ -16,11 +16,11 @@ function About() {
 
                     <h2 className="text-center text-2xl font-fancy mt-[1rem] mb-[1rem] text-indigo-400 max-w-[95%] m-auto font-semibold">I am scared of everything</h2>
 
-                    <p className="text-lg font-medium">This is how I handle my extreme fear of my laptop dying - by planning for the eventuality and providing myself with a roadmap.</p>
+                    <p className="text-lg font-medium text-center">This is how I handle my extreme fear of my laptop dying - by planning for the eventuality and providing myself with a roadmap.</p>
 
                     <h2 className="text-center text-2xl font-fancy mt-[1rem] mb-[1rem] text-indigo-400 max-w-[95%] m-auto font-semibold">It's all going to be ok</h2>
 
-                    <p className="text-lg font-medium">That's all there is to this site. I wanted something for myself, but I figured it might help other people too, so I put everything together and made it a bit pretty. This is by NO means exhaustive but I feel it's enough of a safety net and a decent place to start.</p>
+                    <p className="text-lg font-medium text-center">That's all there is to this site. I wanted something for myself, but I figured it might help other people too, so I put everything together and made it a bit pretty. This is by NO means exhaustive but I feel it's enough of a safety net and a decent place to start.</p>
 
                     <blockquote className="text-center text-2xl font-fancy mt-[1rem] mb-[1rem] text-indigo-400 italic max-w-[95%] m-auto font-semibold">"sometimes good things fall apart so better things can fall together"<br></br>- Marilyn Monroe</blockquote>
                 </div>
